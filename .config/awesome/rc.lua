f1e533d944dfe84ae345b3bc4d9521c64fee73de
Aramande <aramande@hackular.com>
2024-05-20 11:12:53 +0200

    Fixing dmenu and adding screen rules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -545,39 +545,15 @@ globalkeys = mytable.join(
     awful.key({ modkey }, "p", function() menubar.show() end,
               {description = "show the menubar", group = "launcher"}),
     --]]
-    --[[ dmenu
+    -- dmenu
+    awful.key({ modkey }, "r", function () awful.spawn("dmenu_run") end ,
+              {description = "run prompt", group = "launcher"})
     awful.key({ modkey }, "x", function ()
             os.execute(string.format("dmenu_run -i -fn 'Monospace' -nb '%s' -nf '%s' -sb '%s' -sf '%s'",
             beautiful.bg_normal, beautiful.fg_normal, beautiful.bg_focus, beautiful.fg_focus))
         end,
         {description = "show dmenu", group = "launcher"}),
-    --]]
-    -- alternatively use rofi, a dmenu-like application with more features
-    -- check https://github.com/DaveDavenport/rofi for more details
-    --[[ rofi
-    awful.key({ modkey }, "x", function ()
-            os.execute(string.format("rofi -show %s -theme %s",
-            'run', 'dmenu'))
-        end,
-        {description = "show rofi", group = "launcher"}),
-    --]]
-    -- Prompt
-    --awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end,
-    --          {description = "run prompt", group = "launcher"}),
-    awful.key({ modkey }, "r", function () awful.spawn("dmenu_run") end ,
-              {description = "run prompt", group = "launcher"}),
 
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"})
-    --]]
 )
 
 clientkeys = mytable.join(
@@ -750,9 +726,22 @@ awful.rules.rules = {
       }, properties = { titlebars_enabled = false }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    -- Set programs to always map to a specific tag on a specific screen.
+    { rule_any = { 
+        class = {
+          "discord",
+          "TelegramDesktop"
+        } 
+      },
+      properties = { screen = 4, tag = "1" }
+    },
+    { rule_any = { 
+        class = {
+          "Proton Mail",
+        } 
+      },
+      properties = { screen = 4, tag = "2" }
+    },
 }
 
 -- }}}
